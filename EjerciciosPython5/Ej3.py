8c458580b13c3fc3647d25afe2588a6fa23c95bb
Arturo Expósito <arturo-exp@hotmail.com>
2020-02-24 16:41:39 +0100

    Commit

--- EjerciciosPython5/Ej3.py
+++ EjerciciosPython5/Ej3.py
@@ -1,5 +1,18 @@
 class Tiempo:
-    def __init__(self, hora, minuto, segundo):
+    def __init__(self, hora, minuto = 15, segundo = 15):
         self.hora = hora
         self.minuto = minuto
         self.segundo = segundo
+
+    def modificarHora(self, hora, minuto, segundo):
+        self.hora = input("Inserta la hora: ")
+        self.minuto = input("Inserta los minutos: ")
+        self.segundo = input("Inserta los segundos: ")
+
+
+
+h = input("Inserta hora: ")
+
+t = Tiempo(h)
+
+print(list(t.__dict__))
